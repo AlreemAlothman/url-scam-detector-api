876d66e556d741332302e566eee38afb61f58ec5
AlreemAlothman <109047182+AlreemAlothman@users.noreply.github.com>
2025-11-29 01:23:01 +0300

    Enhance FastAPI app with dynamic port and logging
    Updated the FastAPI application to allow dynamic port configuration and improved logging.

--- main.py
+++ main.py
@@ -178,9 +178,14 @@ async def predict_url(data: URLInput, request: Request):
 # لتشغيله مباشرة بـ python main.py
 if __name__ == "__main__":
     import uvicorn
+    import os
+
+    port = int(os.environ.get("PORT", 8000))
+
     uvicorn.run(
         "main:app",
-        host="127.0.0.1",
-        port=8000,
-        reload=True,
+        host="0.0.0.0",
+        port=port,
+        reload=False
     )
+
